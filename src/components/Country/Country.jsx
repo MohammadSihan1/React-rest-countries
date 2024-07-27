4c75d79272f0604429d9cb6d3ef5c359e3aa3758
MohammadSihan1 <mohammadsihan289@gmail.com>
2024-07-27 17:44:16 +0600

    flags added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -2,10 +2,11 @@ import './Country.css'
 
 const Country = ({country}) => {
     console.log(country);
-    const {name} =  country;
+    const {name, flags} =  country;
     return (
         <div className='style'>
             <h3>{name.common}</h3>
+            <img src={flags.svg} width={'200px'} alt="" />
         </div>
     );
 };
